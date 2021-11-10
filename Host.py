2a37fe0a4a3c5b3c326fcd5764f65ebe8f07d7f0
broxy7 <philipp.steigerwald72@gmail.com>
2021-11-10 10:03:56 +0100

    First Push

---
+++ Host.py
@@ -0,0 +1,43 @@
+import serial
+import time
+from servo import servo
+from datetime import datetime
+
+
+def serial_handler_1(ser):
+    print(ser.name)  # check which port was really used
+    time.sleep(10)      # sleep 10 sec
+    message = "M117 MAEE ready\r\n"
+    ser.write(message.encode())
+    # ser.write(b"M117 Helloy\r\n")
+    time.sleep(2) # sleep 2 sec
+    servo()
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+    time.sleep(1)
+    ser.close()
+
+def serial_handler(ser):
+    # servo()
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    if current_time == 0:
+        pass
+    time_message = "M117 " + current_time + "\r\n"
+    ser.write(time_message.encode())
+    # print("Current Time =", current_time)
+    time.sleep(1)
+
+
+ser = serial.Serial('/dev/ttyUSB0', 250000)  # open serial port
+print(ser.name)  # check which port was really used
+time.sleep(10)      # sleep 10 sec
+message = "M117 MAEE ready\r\n"
+ser.write(message.encode())
+# ser.write(b"M117 Helloy\r\n")
+time.sleep(2)  # sleep 2 sec
+while True:
+    serial_handler(ser)
+
+ser.close()
---
+++ servo.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+def servo():
+
+  servoPIN = 17
+  GPIO.setmode(GPIO.BCM)
+  GPIO.setup(servoPIN, GPIO.OUT)
+
+  p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
+  p.start(5) # Initialisierung
+  try:
+    p.ChangeDutyCycle(0)
+    time.sleep(1)
+    p.ChangeDutyCycle(5)
+    time.sleep(1)
+    p.ChangeDutyCycle(10)
+    time.sleep(1)
+    p.ChangeDutyCycle(5)
+    time.sleep(1)
+    p.stop()
+    GPIO.cleanup()
+
+  except KeyboardInterrupt:
+    p.stop()
+    GPIO.cleanup()
+
