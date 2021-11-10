39fb2e4af8219350a07f3965bb999f81c017595d
broxy7 <philipp.steigerwald72@gmail.com>
2021-11-10 11:47:25 +0100

    add move_list

--- Host.py
+++ Host.py
@@ -18,14 +18,21 @@ def serial_handler_1(ser):
     time.sleep(1)
     ser.close()
 
+move_list = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"]
+move_list = ["50", "51", "52", "53", "54", "55", "56", "57", "58", "59"]
+
 def serial_handler(ser, servo_motor):
     # servo()
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
+    current_hour = now.strftime("%M")
+    current_min = now.strftime("%M")
 
-    if current_time == "11:26:20":
+    if current_min in move_list:
+        print(current_min)
         print("Servo move")
         servo_motor.move_angle(10)
+        servo_motor.move_angle(0)
     else:
         time_message = "M117 " + current_time + "\r\n"
         ser.write(time_message.encode())
