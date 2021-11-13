cd12d448d73caaf5d71c2b34307c10f36302776b
broxy7 <philipp.steigerwald72@gmail.com>
2021-11-13 11:49:01 +0100

    test

--- Host.py
+++ Host.py
@@ -39,12 +39,12 @@ def serial_handler(ser, servo_motor):
         #servo_motor.move_duty_cycle(1)
         #time.sleep(1)
 
-        servo_motor.move_angle(90)
-        time.sleep(1)
-        servo_motor.move_angle(45)
-        time.sleep(1)
-        servo_motor.move_angle(135)
-        time.sleep(1)
+        servo_motor.move_angle(110)
+        time.sleep(2)
+        servo_motor.move_angle(120)
+        time.sleep(2)
+        servo_motor.move_angle(0)
+        time.sleep(2)
         servo_motor.move_angle(0)
         #servo_motor.move_duty_cycle(1)
     else:
