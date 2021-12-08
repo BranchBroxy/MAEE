fc515cdd67678da4c7c33b26a61f657e6b625fb4
broxy7 <philipp.steigerwald72@gmail.com>
2021-12-08 20:46:46 +0100

    error fixing

--- xyz.py
+++ xyz.py
@@ -4,8 +4,10 @@ class move_class():
 
     def init_head(self, serial):
         homing_string = "G28" + "\r\n"
+        print(homing_string)
         serial.write(homing_string.encode()) # homing
 
     def move_head(self, serial, x, y, z):
-        move_string = "G0" + "X" + x + "Y" + y + "Z" + z + "\r\n"
+        move_string = "G0" + "X" + str(x) + "Y" + str(y) + "Z" + str(z) + "\r\n"
+        print(move_string)
         serial.write(move_string.encode())
