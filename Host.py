8eadb09ff8f440774f5d4b45cf4768d913d14588
broxy7 <philipp.steigerwald72@gmail.com>
2022-01-20 18:29:16 +0100

    adding Workflow

--- Host.py
+++ Host.py
@@ -8,7 +8,7 @@ move_list = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09"]
 # move_list_min = ["10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
 move_list_min = list(range(00, 60))
 move_list_min = ''.join(str(e) for e in move_list_min)
-move_list_sec = ["00", "15", "30", "45"]
+move_list_sec = ["00"]
 move_list_day = ["Monday", "Wednesday", "Thursday", "Friday"]
 
 def serial_handler(ser, servo_motor, head):
